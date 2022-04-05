b26f805ff059e5fef4ceee095fb5b58fc6dc7047
Daniel Blankenberg <dan.blankenberg@gmail.com>
2022-04-05 11:35:38 -0400

    Update to version 0.1.1

--- naltorfs/codon_freq_from_bicodons.py
+++ naltorfs/codon_freq_from_bicodons.py
@@ -2,6 +2,9 @@
 
 import argparse
 
+from importlib.metadata import version
+from sys import exit
+
 from Bio.Seq import translate
 
 # headers = "Division Assembly    Taxid   Species Organelle   Translation Table   # CDS   # Codon Pairs"
@@ -10,41 +13,46 @@ HEADER_COUNT = 8
 
 def __main__():
     parser = argparse.ArgumentParser(
-        description='Get Codon frequency from bicodon frames')
+        description="Get Codon frequency from bicodon frames"
+    )
     parser.add_argument(
-                        '--bicodons', default='o586358-Refseq_Bicod.tsv',
-                        help="Bicondon rates input")
+        "--bicodons", default="o586358-Refseq_Bicod.tsv", help="Bicondon rates input"
+    )
+    parser.add_argument("--taxid", default="9606", help="tax id")
+    parser.add_argument("--organelle", default="genomic", help="tax id")
+    parser.add_argument("--out", default=None, help="Path to output")
+    parser.add_argument("--aa_out", default=None, help="Path to AA output")
     parser.add_argument(
-                        '--taxid', default='9606',
-                        help='tax id')
-    parser.add_argument(
-                        '--organelle', default='genomic',
-                        help='tax id')
-    parser.add_argument(
-                        '--out', default=None,
-                        help='Path to output')
-    parser.add_argument(
-                        '--aa_out', default=None,
-                        help='Path to AA output')
+        "--version", action="store_true", help="Report version and exit"
+    )
     args = parser.parse_args()
+
+    if args.version:
+        print("nAltORFs {}".format(version("naltorfs")))
+        exit()
 
     codons1 = {}
     codons2 = {}
     aa1 = {}
     aa2 = {}
-    with open(args.bicodons, 'r') as fh:
+    with open(args.bicodons, "r") as fh:
         header = fh.readline().strip().split("\t")
-        taxid_index = header.index('Taxid')
-        organelle_index = header.index('Organelle')
+        taxid_index = header.index("Taxid")
+        organelle_index = header.index("Organelle")
         # codon_pairs_index = header.index('# Codon Pairs')
         bicodons = header[HEADER_COUNT:]
         print("Using %i codon pairs" % (len(bicodons)))
         # TODO: should we confirm all codon pairs are actually listed in header?
         for line in fh:
             line = line.strip().split("\t")
-            if line[taxid_index] == args.taxid and line[organelle_index] == args.organelle:
+            if (
+                line[taxid_index] == args.taxid
+                and line[organelle_index] == args.organelle
+            ):
                 bicodon_counts = line[HEADER_COUNT:]
-                assert len(bicodons) == len(bicodon_counts), "Bicodons and bicodons counts are not same length"
+                assert len(bicodons) == len(
+                    bicodon_counts
+                ), "Bicodons and bicodons counts are not same length"
                 for bicodon, count in zip(bicodons, bicodon_counts):
                     codon = bicodon[1:4]
                     count = int(count)
@@ -69,9 +77,11 @@ def __main__():
     c2_keys = list(codons2.keys())
     c_keys = list(set(c1_keys + c2_keys))
     c_keys.sort()
-    print('Reporting %i codons' % len(c_keys))
-    assert len(c1_keys) == len(c2_keys) == len(c_keys), 'Mismatching key lengths of codons'
-    with open(args.out, 'w') as fh:
+    print("Reporting %i codons" % len(c_keys))
+    assert (
+        len(c1_keys) == len(c2_keys) == len(c_keys)
+    ), "Mismatching key lengths of codons"
+    with open(args.out, "w") as fh:
         fh.write("#frame\tcodon_count\t%s\n" % ("\t".join(c_keys)))
         codons = {}
         for key, value in codons1.items():
@@ -84,11 +94,11 @@ def __main__():
         percent = 0
         fh.write("2-percent\t%i" % codon_count)
         for key in c_keys:
-            p = (codons[key]/codon_count*100)
+            p = codons[key] / codon_count * 100
             fh.write("\t%.28f" % (p))
             percent += p
         fh.write("\n")
-        print('Found a total of %f percent for 2.' % percent)
+        print("Found a total of %f percent for 2." % percent)
 
         codons = {}
         for key, value in codons2.items():
@@ -101,19 +111,21 @@ def __main__():
         percent = 0
         fh.write("3-percent\t%i" % codon_count)
         for key in c_keys:
-            p = (codons[key]/codon_count*100)
+            p = codons[key] / codon_count * 100
             fh.write("\t%.28f" % (p))
             percent += p
         fh.write("\n")
-        print('Found a total of %f percent for 3.' % percent)
+        print("Found a total of %f percent for 3." % percent)
 
     aa1_keys = list(aa1.keys())
     aa2_keys = list(aa2.keys())
     aa_keys = list(set(aa1_keys + aa2_keys))
     aa_keys.sort()
-    print('Reporting %i amino acids' % len(aa_keys))
-    assert len(aa1_keys) == len(aa2_keys) == len(aa_keys), 'Mismatching key lengths of amino acids'
-    with open(args.aa_out, 'w') as fh:
+    print("Reporting %i amino acids" % len(aa_keys))
+    assert (
+        len(aa1_keys) == len(aa2_keys) == len(aa_keys)
+    ), "Mismatching key lengths of amino acids"
+    with open(args.aa_out, "w") as fh:
         fh.write("#frame\taa_count\t%s\n" % ("\t".join(aa_keys)))
         aas = aa1
         aa_count = sum(aas.values())
@@ -124,11 +136,11 @@ def __main__():
         percent = 0
         fh.write("2-percent\t%i" % aa_count)
         for key in aa_keys:
-            p = (aas[key]/aa_count*100)
+            p = aas[key] / aa_count * 100
             fh.write("\t%.28f" % (p))
             percent += p
         fh.write("\n")
-        print('Found a total of %f percent for 2-aa.' % percent)
+        print("Found a total of %f percent for 2-aa." % percent)
 
         aas = aa2
         aa_count = sum(aas.values())
@@ -139,11 +151,11 @@ def __main__():
         percent = 0
         fh.write("3-percent\t%i" % aa_count)
         for key in aa_keys:
-            p = (aas[key]/aa_count*100)
+            p = aas[key] / aa_count * 100
             fh.write("\t%.28f" % (p))
             percent += p
         fh.write("\n")
-        print('Found a total of %f percent for 3-aa.' % percent)
+        print("Found a total of %f percent for 3-aa." % percent)
 
 
 if __name__ == "__main__":
--- naltorfs/find_nested_alt_orfs.py
+++ naltorfs/find_nested_alt_orfs.py
@@ -2,36 +2,55 @@
 
 import argparse
 
-from sys import stderr
+from importlib.metadata import version
+from sys import exit, stderr
 
 from Bio.Seq import reverse_complement, translate
 
 from twobitreader import TwoBitFile
 
-FA_SEP = '|'
+FA_SEP = "|"
 
 log_wtr = stderr
 
 
 def get_bed_from_line(line, log_wtr=log_wtr):
-    fields = line.rstrip('\r\n').split('\t')
+    fields = line.rstrip("\r\n").split("\t")
     if len(fields) < 12:
         raise Exception("BADBED: Not a proper 12 column BED line (%s)." % line)
-    (chrom, chromStart, chromEnd, name, score, strand,
-        thickStart, thickEnd, itemRgb,
-        blockCount, blockSizes, blockStarts) = fields[0:12]
-    bed = BedInterval(chrom=chrom, chromStart=chromStart, chromEnd=chromEnd,
-                      name=name, score=score, strand=strand,
-                      thickStart=thickStart, thickEnd=thickEnd,
-                      itemRgb=itemRgb,
-                      blockCount=blockCount,
-                      blockSizes=blockSizes,
-                      blockStarts=blockStarts,
-                      log_wtr=log_wtr)
+    (
+        chrom,
+        chromStart,
+        chromEnd,
+        name,
+        score,
+        strand,
+        thickStart,
+        thickEnd,
+        itemRgb,
+        blockCount,
+        blockSizes,
+        blockStarts,
+    ) = fields[0:12]
+    bed = BedInterval(
+        chrom=chrom,
+        chromStart=chromStart,
+        chromEnd=chromEnd,
+        name=name,
+        score=score,
+        strand=strand,
+        thickStart=thickStart,
+        thickEnd=thickEnd,
+        itemRgb=itemRgb,
+        blockCount=blockCount,
+        blockSizes=blockSizes,
+        blockStarts=blockStarts,
+        log_wtr=log_wtr,
+    )
     return bed
 
 
-def make_int_list(obj, sep=','):
+def make_int_list(obj, sep=","):
     if obj is None:
         return None
     if isinstance(obj, list):
@@ -51,37 +70,61 @@ def get_all_canonical_sequences(input_rdr, twobit, log_wtr=log_wtr):
                 rval.append(cds)
         except Exception as e:
             print("Cannot determine CDS for exclusion: %s" % str(e), file=log_wtr)
-    print("Excluding %i unique canonical coding sequences from %i BED lines." % (len(rval), i), file=log_wtr)
+    print(
+        "Excluding %i unique canonical coding sequences from %i BED lines."
+        % (len(rval), i),
+        file=log_wtr,
+    )
     input_rdr.seek(tell)
     return rval
 
 
 class BedInterval(object):
-    def __init__(self, chrom=None, chromStart=None, chromEnd=None,
-                 name=None, score=None, strand=None,
-                 thickStart=None, thickEnd=None, itemRgb=None,
-                 blockCount=None, blockSizes=None, blockStarts=None, log_wtr=log_wtr):
+    def __init__(
+        self,
+        chrom=None,
+        chromStart=None,
+        chromEnd=None,
+        name=None,
+        score=None,
+        strand=None,
+        thickStart=None,
+        thickEnd=None,
+        itemRgb=None,
+        blockCount=None,
+        blockSizes=None,
+        blockStarts=None,
+        log_wtr=log_wtr,
+    ):
         self.chrom = chrom
         self.chromStart = int(chromStart)
         self.chromEnd = int(chromEnd)
         self.name = name
         self.score = int(score) if score is not None else 0
-        self.strand = '-' if str(strand).startswith('-') else '+'
+        self.strand = "-" if str(strand).startswith("-") else "+"
         self.thickStart = int(thickStart) if thickStart else self.chromStart
         self.thickEnd = int(thickEnd) if thickEnd else self.chromEnd
-        self.itemRgb = str(itemRgb) if itemRgb is not None else r'100,100,100'
+        self.itemRgb = str(itemRgb) if itemRgb is not None else r"100,100,100"
         self.blockCount = int(blockCount)
         self.blockSizes = make_int_list(blockSizes)
         self.blockStarts = make_int_list(blockStarts)
         self.log_wtr = log_wtr
 
     def __str__(self):
-        return '%s\t%d\t%d\t%s\t%d\t%s\t%d\t%d\t%s\t%d\t%s\t%s' % (
-            self.chrom, self.chromStart, self.chromEnd,
-            self.name, self.score, self.strand,
-            self.thickStart, self.thickEnd, str(self.itemRgb), self.blockCount,
-            ','.join([str(x) for x in self.blockSizes]),
-            ','.join([str(x) for x in self.blockStarts]))
+        return "%s\t%d\t%d\t%s\t%d\t%s\t%d\t%d\t%s\t%d\t%s\t%s" % (
+            self.chrom,
+            self.chromStart,
+            self.chromEnd,
+            self.name,
+            self.score,
+            self.strand,
+            self.thickStart,
+            self.thickEnd,
+            str(self.itemRgb),
+            self.blockCount,
+            ",".join([str(x) for x in self.blockSizes]),
+            ",".join([str(x) for x in self.blockStarts]),
+        )
 
     def get_cds_bed(self):
         trimmed = self.trim_by_region(self.thickStart, self.thickEnd)
@@ -106,15 +149,25 @@ class BedInterval(object):
         block_starts = [x - region_start for x in starts]
         block_sizes = [x - y for x, y in zip(ends, starts)]
 
-        return BedInterval(chrom=self.chrom, chromStart=region_start, chromEnd=region_end,
-                           name=self.name, score=self.score, strand=self.strand,
-                           thickStart=thick_start, thickEnd=thick_end, itemRgb=self.itemRgb,
-                           blockCount=len(block_sizes), blockSizes=block_sizes, blockStarts=block_starts,
-                           log_wtr=self.log_wtr)
+        return BedInterval(
+            chrom=self.chrom,
+            chromStart=region_start,
+            chromEnd=region_end,
+            name=self.name,
+            score=self.score,
+            strand=self.strand,
+            thickStart=thick_start,
+            thickEnd=thick_end,
+            itemRgb=self.itemRgb,
+            blockCount=len(block_sizes),
+            blockSizes=block_sizes,
+            blockStarts=block_starts,
+            log_wtr=self.log_wtr,
+        )
 
     def trim_by_cds_offsets(self, start_offset, end_offset):
         cds_bed = self.get_cds_bed()
-        if cds_bed.strand != '+':
+        if cds_bed.strand != "+":
             t = start_offset
             start_offset = end_offset
             end_offset = t
@@ -154,7 +207,10 @@ class BedInterval(object):
         return cds_bed.trim_by_region(start, end)
 
     def get_cdna_exon_regions(self):
-        return [(self.chromStart + x, self.chromStart + x + y) for x, y in zip(self.blockStarts, self.blockSizes)]
+        return [
+            (self.chromStart + x, self.chromStart + x + y)
+            for x, y in zip(self.blockStarts, self.blockSizes)
+        ]
 
     def get_coding_exon_regions(self):
         starts = []
@@ -169,91 +225,127 @@ class BedInterval(object):
                 ends.append(end)
         return [(x, y) for x, y in zip(starts, ends)]
 
-    def get_sequence_for_region(self, chrom, start, end, twobit, strand='+'):
+    def get_sequence_for_region(self, chrom, start, end, twobit, strand="+"):
         seq = None
         if chrom in twobit and 0 <= start < end < len(twobit[chrom]):
             seq = twobit[chrom][start:end]
-        contig = chrom[3:] if chrom.startswith('chr') else 'chr%s' % chrom
+        contig = chrom[3:] if chrom.startswith("chr") else "chr%s" % chrom
         if contig in twobit and 0 <= start < end < len(twobit[contig]):
             seq = twobit[contig][start:end]
         if not seq:
-            print("Seq is None (%s): %s. %s %s %s %s" % (seq, str(self), chrom, start, end, strand), file=self.log_wtr)
-        elif strand != '+':
+            print(
+                "Seq is None (%s): %s. %s %s %s %s"
+                % (seq, str(self), chrom, start, end, strand),
+                file=self.log_wtr,
+            )
+        elif strand != "+":
             seq = reverse_complement(seq)
         return seq
 
-    def get_sequence_for_regions(self, chrom, regions, twobit, strand='+'):
-        seq = [self.get_sequence_for_region(chrom, start, end, twobit, strand=strand) for start, end in regions]
+    def get_sequence_for_regions(self, chrom, regions, twobit, strand="+"):
+        seq = [
+            self.get_sequence_for_region(chrom, start, end, twobit, strand=strand)
+            for start, end in regions
+        ]
         if None in seq:
             return None  # most likely the chromosome does not exist in twobit
-        if strand != '+':
+        if strand != "+":
             seq.reverse()
         return "".join(seq)
 
     def get_sequence(self, twobit):
-        return self.get_sequence_for_region(self.chrom, self.chromStart, self.chromEnd, twobit, strand=self.strand)
+        return self.get_sequence_for_region(
+            self.chrom, self.chromStart, self.chromEnd, twobit, strand=self.strand
+        )
 
     def get_cdna(self, twobit):
         regions = self.get_cdna_exon_regions()
-        return (regions, self.get_sequence_for_regions(self.chrom, regions, twobit, strand=self.strand))
+        return (
+            regions,
+            self.get_sequence_for_regions(
+                self.chrom, regions, twobit, strand=self.strand
+            ),
+        )
 
     def get_cds(self, twobit):
         regions = self.get_coding_exon_regions()
-        return (regions, self.get_sequence_for_regions(self.chrom, regions, twobit, strand=self.strand))
+        return (
+            regions,
+            self.get_sequence_for_regions(
+                self.chrom, regions, twobit, strand=self.strand
+            ),
+        )
 
 
 def __main__():
     parser = argparse.ArgumentParser(
-        description='Find LNO ORFs')
+        description="Find Nested Alternate ORFs (nAlt-ORFs)"
+    )
+    parser.add_argument("--bed", default=None, help="BED input")
     parser.add_argument(
-        '--bed', default=None,
-        help="BED input")
+        "-t", "--twobit", default=None, help="Genome reference sequence in 2bit format"
+    )
     parser.add_argument(
-        '-t', '--twobit', default=None,
-        help='Genome reference sequence in 2bit format')
+        "-f",
+        "--peptide_fasta_out",
+        default=None,
+        help="Path to output peptide_fasta_out.fasta",
+    )
     parser.add_argument(
-        '-f', '--peptide_fasta_out', default=None,
-        help='Path to output peptide_fasta_out.fasta')
+        "-n",
+        "--naltorfs_fasta_out",
+        default=None,
+        help="Path to output naltorfs_fasta_out.fasta",
+    )
     parser.add_argument(
-        '-n', '--lno_fasta_out', default=None,
-        help='Path to output lno_fasta_out.fasta')
+        "-o", "--cds_fasta_out", default=None, help="Path to output cds_fasta_out.fasta"
+    )
     parser.add_argument(
-        '-o', '--cds_fasta_out', default=None,
-        help='Path to output cds_fasta_out.fasta')
+        "-b", "--bed_out", default=None, help="Path to output probed.bed"
+    )
+    parser.add_argument("--log", default=None, help="Path to output log")
     parser.add_argument(
-        '-b', '--bed_out', default=None,
-        help='Path to output probed.bed')
+        "-m",
+        "--min_length",
+        type=int,
+        default=10,
+        help="Minimum length of protein translation to report",
+    )
+    parser.add_argument("-r", "--reference", default="", help="Genome Reference Name")
     parser.add_argument(
-        '--log', default=None,
-        help='Path to output log')
+        "--unique_sequences",
+        action="store_true",
+        default=False,
+        help="Only report the first unique occurrence of an alternate sequence",
+    )
     parser.add_argument(
-        '-m', '--min_length', type=int, default=10,
-        help='Minimum length of protein translation to report')
+        "--no_canonical_cds",
+        default=None,
+        help="Do not report any alternate sequences that match a provided cannonical CDS in file",
+    )
     parser.add_argument(
-        '-r', '--reference', default='',
-        help='Genome Reference Name')
-    parser.add_argument(
-        '--unique_sequences', action='store_true', default=False,
-        help='Only report the first unique occurrence of an alternate sequence')
-    parser.add_argument(
-        '--no_canonical_cds', default=None,
-        help='Do not report any alternate sequences that match a provided cannonical CDS in file')
+        "--version", action="store_true", help="Report version and exit"
+    )
     args = parser.parse_args()
 
-    input_rdr = open(args.bed, 'r')
-    peptide_fasta_wtr = open(args.peptide_fasta_out, 'w')
-    lno_fasta_wtr = open(args.lno_fasta_out, 'w')
-    cds_fasta_wtr = open(args.cds_fasta_out, 'w')
+    if args.version:
+        print("nAltORFs {}".format(version("naltorfs")))
+        exit()
 
-    log_wtr = open(args.log, 'w')
+    input_rdr = open(args.bed, "r")
+    peptide_fasta_wtr = open(args.peptide_fasta_out, "w")
+    lno_fasta_wtr = open(args.naltorfs_fasta_out, "w")
+    cds_fasta_wtr = open(args.cds_fasta_out, "w")
 
-    bed_wtr = open(args.bed_out, 'w')
+    log_wtr = open(args.log, "w")
+
+    bed_wtr = open(args.bed_out, "w")
 
     twobit = TwoBitFile(args.twobit)
     print(twobit.sequence_sizes(), file=log_wtr)
 
     if args.no_canonical_cds:
-        with open(args.no_canonical_cds, 'r') as fh:
+        with open(args.no_canonical_cds, "r") as fh:
             unique_seqs = get_all_canonical_sequences(fh, twobit, log_wtr)
     else:
         unique_seqs = []
@@ -280,23 +372,31 @@ def __main__():
             is_unique = is_always_true
 
     def write_outputs(tbed, accession, peptide, nucseq, cds_bed, cds):
-        probed = "%s\t%s\t%s\t%s%s" % (accession, peptide,
-                                       'unique', args.reference,
-                                       '\t.' * 9)
+        probed = "%s\t%s\t%s\t%s%s" % (
+            accession,
+            peptide,
+            "unique",
+            args.reference,
+            "\t." * 9,
+        )
         if bed_wtr:
             bed_wtr.write("%s\t%s\n" % (str(tbed), probed))
             bed_wtr.flush()
-        location = "chromosome:%s:%s:%s:%s:%s"\
-            % (args.reference, tbed.chrom,
-               tbed.thickStart, tbed.thickEnd, tbed.strand)
-        fa_desc = '%s%s' % (FA_SEP, location)
+        location = "chromosome:%s:%s:%s:%s:%s" % (
+            args.reference,
+            tbed.chrom,
+            tbed.thickStart,
+            tbed.thickEnd,
+            tbed.strand,
+        )
+        fa_desc = "%s%s" % (FA_SEP, location)
         fa_id = ">%s%s\n" % (tbed.name, fa_desc)
         peptide_fasta_wtr.write(fa_id)
         peptide_fasta_wtr.write(peptide)
         peptide_fasta_wtr.write("\n")
         peptide_fasta_wtr.flush()
 
-        fa_desc = '%s%s' % (FA_SEP, location)
+        fa_desc = "%s%s" % (FA_SEP, location)
         fa_id = ">%s%s\n" % (tbed.name, fa_desc)
         lno_fasta_wtr.write(fa_id)
         lno_fasta_wtr.write(nucseq)
@@ -304,7 +404,7 @@ def __main__():
         lno_fasta_wtr.flush()
 
         # fa_desc = '%s%s' % (args.fa_sep, location)
-        fa_desc = ''
+        fa_desc = ""
         fa_id = ">%s%s\n" % (cds_bed.name, fa_desc)
         cds_fasta_wtr.write(fa_id)
         cds_fasta_wtr.write(cds)
@@ -326,16 +426,19 @@ def __main__():
             return 0
         cds_len = len(cds_seq)
         if cds_len % 3 != 0:
-            cds_seq = cds_seq[:-(cds_len % 3)]
-            print("cds length is not divisible by 3!!!: %d. %s" % (cds_len, str(bed)), file=log_wtr)  # why does this happen? check several manually, the records may be listed as incomplete
+            cds_seq = cds_seq[: -(cds_len % 3)]
+            print(
+                "cds length is not divisible by 3!!!: %d. %s" % (cds_len, str(bed)),
+                file=log_wtr,
+            )  # why does this happen? check several manually, the records may be listed as incomplete
 
         cds_bed = bed.get_cds_bed()
         alt_cds = cds_seq[1:]  # original frame is now third (2)
         alt_cds_upper = alt_cds.upper()
 
         cds_offset = 1
-        if 'ATG' in alt_cds_upper:
-            atg_index = alt_cds_upper.find('ATG')
+        if "ATG" in alt_cds_upper:
+            atg_index = alt_cds_upper.find("ATG")
             atg_mod = atg_index % 3
             if atg_mod == 2:
                 return 0  # is is original frame
@@ -350,8 +453,10 @@ def __main__():
             alt_cds = alt_cds[:-alt_cds_mod]
 
             aaseq = translate(alt_cds)
-            assert aaseq.startswith('M'), Exception('Translation did not start with methionine: %s --> %s' % alt_cds, aaseq)
-            aa_end = aaseq.find('*')
+            assert aaseq.startswith("M"), Exception(
+                "Translation did not start with methionine: %s --> %s" % alt_cds, aaseq
+            )
+            aa_end = aaseq.find("*")
             if aa_end < 0:
                 aa_end = len(aaseq)
             aaseq = aaseq[:aa_end]
@@ -361,7 +466,12 @@ def __main__():
                 alt_cds_end_trim = cds_len - alt_cds_len - cds_offset
                 alt_cds = alt_cds[:alt_cds_len]
                 tbed = cds_bed.trim_by_cds_offsets(cds_offset, alt_cds_end_trim)
-                prot_acc = "%s_f%d_%d_%d" % (transcript_id, frame, cds_offset, alt_cds_end_trim)  # tbed.chromStart, tbed.chromEnd)
+                prot_acc = "%s_f%d_%d_%d" % (
+                    transcript_id,
+                    frame,
+                    cds_offset,
+                    alt_cds_end_trim,
+                )  # tbed.chromStart, tbed.chromEnd)
                 if is_unique(alt_cds):
                     translate_count += 1
                     write_outputs(tbed, prot_acc, aaseq, alt_cds, cds_bed, cds_seq)
@@ -379,10 +489,13 @@ def __main__():
                 transcript_count += 1
                 lno_count += find_lno_orf(bed)
             except Exception as e:
-                print("BED format Error: line %d: %s\n%s"
-                      % (i, bedline, e), file=log_wtr)
-        print("transcripts: %d\tunique LNO ORFs: %d"
-              % (transcript_count, lno_count), file=log_wtr)
+                print(
+                    "BED format Error: line %d: %s\n%s" % (i, bedline, e), file=log_wtr
+                )
+        print(
+            "transcripts: %d\tunique LNO ORFs: %d" % (transcript_count, lno_count),
+            file=log_wtr,
+        )
 
 
 if __name__ == "__main__":
